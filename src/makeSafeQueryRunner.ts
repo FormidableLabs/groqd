6b9da46c4d2bd97b83baaa49156c5c7404bae823
Grant Sander <gksander93@gmail.com>
2023-01-27 08:05:23 -0600

    Shuffle types a bit

--- src/makeSafeQueryRunner.ts
+++ src/makeSafeQueryRunner.ts
@@ -4,8 +4,6 @@ import { BaseQuery } from "./builder";
 /**
  * Utility to create a "query runner" that consumes the result of the `q` function.
  */
-type BaseType<T = any> = z.ZodType<T>;
-
 export const makeSafeQueryRunner =
   <Fn extends (query: string, ...rest: any[]) => Promise<any>>(fn: Fn) =>
   <T extends BaseType>(
@@ -14,6 +12,7 @@ export const makeSafeQueryRunner =
   ): Promise<z.infer<T>> =>
     fn(query, ...rest).then((res) => schema.parse(res));
 
+type BaseType<T = any> = z.ZodType<T>;
 type ButFirst<T extends unknown[]> = T extends [unknown, ...infer U]
   ? U
   : never;
