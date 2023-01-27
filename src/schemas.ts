067f905ee73ea64d9338858bbd204694eb984166
Grant Sander <gksander93@gmail.com>
2023-01-26 21:01:03 -0600

    Initial setup for Block Schema

--- src/schemas.ts
+++ src/schemas.ts
@@ -1,9 +1,42 @@
 import { z } from "zod";
 
+/**
+ * Custom date schema that will parse date strings to Date objects
+ */
 const dateSchema = () =>
   z.preprocess((arg) => {
     if (typeof arg == "string" || arg instanceof Date) return new Date(arg);
   }, z.date());
+
+/**
+ * Content block schema for standard content blocks.
+ */
+const contentBlock = () =>
+  z.object({
+    _type: z.literal("block"),
+    _key: z.string().optional(),
+    children: z.array(
+      z.object({
+        _key: z.string(),
+        _type: z.string(),
+        text: z.string(),
+        marks: z.array(z.string()),
+      })
+    ),
+    markDefs: z
+      .array(
+        z
+          .object({
+            _type: z.string(),
+            _key: z.string(),
+          })
+          .catchall(z.unknown())
+      )
+      .optional(),
+    style: z.string().optional(),
+    listItem: z.string().optional(),
+    level: z.number().optional(),
+  });
 
 export const schemas = {
   string: z.string,
@@ -17,4 +50,5 @@ export const schemas = {
   union: z.union,
   array: z.array,
   object: z.object,
+  contentBlock,
 };
