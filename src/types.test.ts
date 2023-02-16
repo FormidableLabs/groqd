3f8d0af0ffbc7134a15893af705cb3416f136f0d
Grant Sander <gksander93@gmail.com>
2023-02-16 06:25:00 -0600

    Functionality for TypeFromSelection

--- src/types.test.ts
+++ src/types.test.ts
@@ -1,6 +1,6 @@
 import { describe, it } from "vitest";
 import { q } from "./index";
-import type { InferType } from "./types";
+import type { InferType, TypeFromSelection, Selection } from "./index";
 
 describe("InferType", () => {
   it("can infer type from query", () => {
@@ -39,5 +39,35 @@ describe("InferType", () => {
     // @ts-expect-error Expecting error, unknown field
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const res3: Res = [{ age: 30 }];
+  });
+});
+
+describe("TypeFromSelection", () => {
+  it("can grab TS type from a selection object", () => {
+    const textBlockSelection = {
+      _type: q.literal("textBlock"),
+      content: q.array(
+        q.object({
+          content: q.string().nullable(),
+          textColor: q
+            .union([q.literal("light"), q.literal("dark")])
+            .nullable()
+            .optional(),
+        })
+      ),
+    } satisfies Selection;
+
+    type Res = TypeFromSelection<typeof textBlockSelection>;
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const res: Res = { _type: "textBlock", content: [] };
+
+    // @ts-expect-error Expecting error, since _type is not of correct type
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const res2: Res = { _type: "foop", content: [] };
+
+    // @ts-expect-error Expecting error, since content is missing
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const res3: Res = { _type: "textBlock" };
   });
 });
