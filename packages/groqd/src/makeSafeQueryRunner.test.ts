4a594a631bf9ae8a264eef96ad3268645c7ee7c0
Grant Sander <gksander93@gmail.com>
2023-03-29 20:42:20 -0500

    Initial move

--- packages/groqd/src/makeSafeQueryRunner.test.ts
+++ packages/groqd/src/makeSafeQueryRunner.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect, vi } from "vitest";
-import { GroqdParseError, q } from ".";
+import { GroqdParseError, q } from "./index";
 import { makeSafeQueryRunner } from "./makeSafeQueryRunner";
 
 describe("makeSafeQueryRunner", () => {
