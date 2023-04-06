9b10da0007232ad0322f72899d2b00f73788aa59
Grant Sander <gksander93@gmail.com>
2023-04-06 14:04:15 -0500

    Update contentBlock.ts

--- packages/groqd/src/contentBlock.ts
+++ packages/groqd/src/contentBlock.ts
@@ -23,24 +23,34 @@ export function contentBlocks({ markDefs }: { markDefs?: z.ZodType } = {}) {
   return z.array(z.object(makeContentBlockQuery(markDefs || baseMarkdefsType)));
 }
 
-export function makeContentBlockQuery<T extends z.ZodType>(markDefs?: T) {
+export function makeContentBlockQuery(): typeof baseContentFields & {
+  markDefs: z.ZodOptional<z.ZodArray<typeof baseMarkdefsType>>;
+};
+export function makeContentBlockQuery<T extends z.ZodType>(
+  markDefs: T
+): typeof baseContentFields & { markDefs: z.ZodOptional<z.ZodArray<T>> };
+export function makeContentBlockQuery(markDefs?: z.ZodType) {
   return {
-    _type: z.string(),
-    _key: z.string().optional(),
-    children: z.array(
-      z.object({
-        _key: z.string(),
-        _type: z.string(),
-        text: z.string(),
-        marks: z.array(z.string()),
-      })
-    ),
-    markDefs: z.array(markDefs === undefined ? z.any() : markDefs).optional(),
-    style: z.string().optional(),
-    listItem: z.string().optional(),
-    level: z.number().optional(),
+    ...baseContentFields,
+    markDefs: z.array(markDefs || baseMarkdefsType).optional(),
   };
 }
+
+const baseContentFields = {
+  _type: z.string(),
+  _key: z.string().optional(),
+  children: z.array(
+    z.object({
+      _key: z.string(),
+      _type: z.string(),
+      text: z.string(),
+      marks: z.array(z.string()),
+    })
+  ),
+  style: z.string().optional(),
+  listItem: z.string().optional(),
+  level: z.number().optional(),
+} as const;
 
 const baseMarkdefsType = z
   .object({
