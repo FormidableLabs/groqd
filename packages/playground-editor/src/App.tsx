16a293d3a3a158d37353a4c1fa832fda9956b7a1
Grant Sander <gksander93@gmail.com>
2023-04-11 09:12:08 -0500

    Initial webpack setup

---
+++ packages/playground-editor/src/App.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export function App() {
+  return (
+    <div>
+      <h1>Hello world</h1>
+    </div>
+  );
+}
---
+++ packages/playground-editor/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "build"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        loader: "ts-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".ts", ".tsx"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "build"),
+    },
+    port: 3000,
+  },
+};
