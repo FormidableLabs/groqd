ac5935ccae08b6b5cb52207986c495f353b0df63
Grant Sander <gksander93@gmail.com>
2023-04-13 12:08:01 -0500

    Fix up CI issues

--- packages/groqd-playground/src/Playground.tsx
+++ packages/groqd-playground/src/Playground.tsx
@@ -1,9 +1,9 @@
-import * as React from 'react';
+import * as React from "react";
 
 export default function Playground() {
-	return (
-		<div>
-			<h1>Hello world!</h1>
-		</div>
-	)
+  return (
+    <div>
+      <h1>Hello world!</h1>
+    </div>
+  );
 }
