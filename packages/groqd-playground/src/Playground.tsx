0049667b18e17629b43da5a41e786eca2d4de1aa
Grant Sander <gksander93@gmail.com>
2023-04-16 20:10:48 -0500

    Use a reducer

--- packages/groqd-playground/src/Playground.tsx
+++ packages/groqd-playground/src/Playground.tsx
@@ -1,22 +1,27 @@
 import * as React from "react";
 import { useClient } from "sanity";
-import { Box, Code, Flex, Stack } from "@sanity/ui";
+import { Box, Code, Flex } from "@sanity/ui";
 import { z } from "zod";
 import * as q from "groqd";
 import { BaseQuery } from "groqd/src/baseQuery";
 import Split from "@uiw/react-split";
 
 export default function GroqdPlayground() {
-  const [query, setQuery] = React.useState<BaseQuery<any>>(q.q(""));
-  const [params, setParams] = React.useState<Record<string, string | number>>(
-    {}
-  );
-  const [response, setResponse] = React.useState("");
+  const [
+    { query, params, parsedResponse, fetchParseError, rawResponse },
+    dispatch,
+  ] = React.useReducer(reducer, { query: q.q("") });
   const client = useClient({ apiVersion: "v2021-10-21" });
 
   const runQuery = React.useRef(
     q.makeSafeQueryRunner((query, params?: Record<string, string | number>) =>
-      client.fetch(query, params)
+      client.fetch(query, params).then((res) => {
+        dispatch({
+          type: "RAW_RESPONSE_RECEIVED",
+          payload: { rawResponse: res },
+        });
+        return res;
+      })
     )
   );
 
@@ -36,8 +41,10 @@ export default function GroqdPlayground() {
                 params?: Record<string, string | number>
               ) => {
                 try {
-                  setQuery(query);
-                  setParams(params || {});
+                  dispatch({
+                    type: "INPUT_EVAL_SUCCESS",
+                    payload: { query, params },
+                  });
                 } catch {}
               },
             },
@@ -72,11 +79,15 @@ export default function GroqdPlayground() {
   const handleRun = async () => {
     try {
       const data = await runQuery.current(query, params);
-      setResponse(JSON.stringify(data, null, 2)); // TODO: JSON explorer
+      dispatch({
+        type: "FETCH_RESPONSE_PARSED",
+        payload: { parsedResponse: data },
+      });
     } catch (err) {
-      if (err instanceof q.GroqdParseError) {
-        setResponse(err.message);
-      }
+      dispatch({
+        type: "FETCH_PARSE_FAILURE",
+        payload: { fetchParseError: err },
+      });
     }
   };
 
@@ -95,25 +106,106 @@ export default function GroqdPlayground() {
           <Box>
             <h3>Query</h3>
             <pre>{query.query}</pre>
-            {Object.keys(params).length > 0 && (
+            {params && Object.keys(params).length > 0 ? (
               <div>
                 <h3>Params</h3>
-                <pre>{JSON.stringify(params, null, 2)}</pre>
+                <ul>
+                  {Object.entries(params).map(([key, value]) => (
+                    <li key={key}>
+                      ${key}: {value}
+                    </li>
+                  ))}
+                </ul>
               </div>
-            )}
+            ) : null}
+
+            <button onClick={handleRun}>RUN QUERY</button>
           </Box>
           <Flex flex={1} direction="column" overflow="hidden">
-            <h3>Query Response</h3>
-            <button onClick={handleRun}>RUN QUERY</button>
-            <Box flex={1} overflow="auto">
-              <Code language="json">{response}</Code>
-            </Box>
+            {fetchParseError ? (
+              <Box flex={1} overflow="auto">
+                <h3>Error:</h3>
+                <pre>
+                  {fetchParseError instanceof Error
+                    ? fetchParseError.message
+                    : "Something went wrong."}
+                </pre>
+                <h3>Raw response:</h3>
+                <Code language="json">
+                  {JSON.stringify(rawResponse, null, 2)}
+                </Code>
+              </Box>
+            ) : (
+              <React.Fragment>
+                <h3>Query Response</h3>
+                <Box flex={1} overflow="auto">
+                  <Code language="json">
+                    {JSON.stringify(parsedResponse, null, 2)}
+                  </Code>
+                </Box>
+              </React.Fragment>
+            )}
           </Flex>
         </Split>
       </Box>
     </Split>
   );
 }
+
+type Params = Record<string, string | number>;
+type State = {
+  query: BaseQuery<any>;
+  params?: Params;
+  rawResponse?: unknown;
+  parsedResponse?: unknown;
+  inputParseError?: Error;
+  fetchParseError?: unknown;
+};
+
+type Action =
+  | {
+      type: "INPUT_EVAL_SUCCESS";
+      payload: { query: BaseQuery<any>; params?: Params };
+    }
+  | { type: "INPUT_PARSE_FAILURE"; payload: { inputParseError: Error } }
+  | { type: "RAW_RESPONSE_RECEIVED"; payload: { rawResponse: unknown } }
+  | { type: "FETCH_RESPONSE_PARSED"; payload: { parsedResponse: unknown } }
+  | {
+      type: "FETCH_PARSE_FAILURE";
+      payload: { fetchParseError: unknown; rawResponse?: unknown };
+    };
+
+const reducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case "INPUT_EVAL_SUCCESS":
+      return {
+        ...state,
+        query: action.payload.query,
+        params: action.payload.params,
+        inputParseError: undefined,
+      };
+    case "INPUT_PARSE_FAILURE":
+      return {
+        ...state,
+        inputParseError: action.payload.inputParseError,
+      };
+    case "RAW_RESPONSE_RECEIVED":
+      return {
+        ...state,
+        rawResponse: action.payload.rawResponse,
+      };
+    case "FETCH_RESPONSE_PARSED":
+      return {
+        ...state,
+        parsedResponse: action.payload.parsedResponse,
+        fetchParseError: undefined,
+      };
+    case "FETCH_PARSE_FAILURE":
+      return { ...state, fetchParseError: action.payload.fetchParseError };
+    default:
+      return state;
+  }
+};
 
 const EDITOR_INITIAL_WIDTH = 500;
 
