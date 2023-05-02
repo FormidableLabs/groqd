6e0c3a7090cfa5a677fecbab9aa6a34c4148bb0f
Grant Sander <gksander93@gmail.com>
2023-05-02 16:39:04 -0500

    Pretty up action list

--- website/src/arcade/ArcadeActionList.tsx
+++ website/src/arcade/ArcadeActionList.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import { Menu, Transition } from "@headlessui/react";
+import { HiChevronDown } from "react-icons/hi";
+import clsx from "clsx";
 
 type ArcadeActionListProps<T> = {
   title: string;
@@ -16,65 +18,53 @@ export const ArcadeActionList = <T,>({
 }: ArcadeActionListProps<T>) => {
   return (
     <Menu as="div" className="relative inline-block">
-      <div>
-        <Menu.Button className="inline-flex w-full justify-center rounded-md bg-black bg-opacity-20 px-4 py-2">
-          {title}
-          {/*<ChevronDownIcon*/}
-          {/*  className="ml-2 -mr-1 h-5 w-5 text-violet-200 hover:text-violet-100"*/}
-          {/*  aria-hidden="true"*/}
-          {/*/>*/}
-        </Menu.Button>
-      </div>
+      {({ open }) => (
+        <React.Fragment>
+          <Menu.Button
+            className={clsx(
+              "inline-flex w-full justify-center items-center rounded-md bg-black px-4 py-2 border-none cursor-pointer",
+              "bg-transparent hover:bg-gray-50 transition-colors duration-150",
+              "text-lg font-bold text-gray-700",
+              open && "text-gray-400"
+            )}
+          >
+            {title}
+            <HiChevronDown className="ml-2" aria-hidden="true" />
+          </Menu.Button>
 
-      <Transition
-        as={React.Fragment}
-        enter="transition ease-out duration-100"
-        enterFrom="transform opacity-0 scale-95"
-        enterTo="transform opacity-100 scale-100"
-        leave="transition ease-in duration-75"
-        leaveFrom="transform opacity-100 scale-100"
-        leaveTo="transform opacity-0 scale-95"
-      >
-        <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-          {items.map((item) => (
-            <div className="px-1 py-1 " key={item.title}>
-              <Menu.Item>
-                {({ active }) => (
-                  <button
-                    className={active ? "bg-red-300" : ""}
-                    onClick={() => onSelectItem(item.value)}
-                  >
-                    {item.title}
-                  </button>
-                )}
-              </Menu.Item>
-            </div>
-          ))}
-        </Menu.Items>
-      </Transition>
-
-      {/*<Menu.Items>*/}
-      {/*  {items.map((item) => (*/}
-      {/*    <Menu.Item key={item.title}>*/}
-      {/*      <span>{item.title}</span>*/}
-      {/*    </Menu.Item>*/}
-      {/*  ))}*/}
-      {/*</Menu.Items>*/}
+          <Transition
+            as={React.Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items className="absolute left-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none flex flex-col gap-2 p-2">
+              {items.map((item) => (
+                <div className="" key={item.title}>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        className={clsx(
+                          "w-full p-2 border-none rounded-md",
+                          "text-left text-base cursor-pointer",
+                          "hover:bg-gray-50",
+                          active ? "bg-gray-50" : "bg-transparent"
+                        )}
+                        onClick={() => onSelectItem(item.value)}
+                      >
+                        {item.title}
+                      </button>
+                    )}
+                  </Menu.Item>
+                </div>
+              ))}
+            </Menu.Items>
+          </Transition>
+        </React.Fragment>
+      )}
     </Menu>
   );
-  // return (
-  //   <DropdownMenu.Root>
-  //     <DropdownMenu.Trigger asChild>
-  //       <button className="bg-red-300" aria-label="Dataset options">
-  //         {title}
-  //       </button>
-  //     </DropdownMenu.Trigger>
-  //
-  //     <DropdownMenu.Portal>
-  //       <DropdownMenu.Content>
-
-  //       </DropdownMenu.Content>
-  //     </DropdownMenu.Portal>
-  //   </DropdownMenu.Root>
-  // );
 };
