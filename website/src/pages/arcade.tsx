985c4a105abfacb210ba29c913717d0160a625c2
Grant Sander <gksander93@gmail.com>
2023-05-05 07:13:13 -0500

    Add meta to arcade

--- website/src/pages/arcade.tsx
+++ website/src/pages/arcade.tsx
@@ -1,20 +1,34 @@
 import * as React from "react";
 import BrowserOnly from "@docusaurus/BrowserOnly";
 import { HiOutlineCog } from "react-icons/hi";
+import Head from "@docusaurus/Head";
 
 export default function ArcadePage() {
   return (
-    <BrowserOnly
-      fallback={
-        <div className="fixed inset-0 flex items-center justify-center">
-          <HiOutlineCog className="text-3xl animate-spin" />
-        </div>
-      }
-    >
-      {() => {
-        const { Arcade } = require("@site/src/arcade/Arcade");
-        return <Arcade />;
-      }}
-    </BrowserOnly>
+    <React.Fragment>
+      <Head>
+        <title>GROQD Arcade</title>
+        <meta
+          name="description"
+          content="Write and test GROQD queries against custom datasets."
+        />
+        <meta
+          name="keywords"
+          content="groqd, sanity, arcade, query, groq, typescript"
+        />
+      </Head>
+      <BrowserOnly
+        fallback={
+          <div className="fixed inset-0 flex items-center justify-center">
+            <HiOutlineCog className="text-3xl animate-spin" />
+          </div>
+        }
+      >
+        {() => {
+          const { Arcade } = require("@site/src/arcade/Arcade");
+          return <Arcade />;
+        }}
+      </BrowserOnly>
+    </React.Fragment>
   );
 }
