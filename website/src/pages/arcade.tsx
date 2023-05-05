e96ac777cc5d0fc438041388e6c61e967f5b0fed
Grant Sander <gksander93@gmail.com>
2023-05-04 20:54:20 -0500

    loading screen colors

--- website/src/pages/arcade.tsx
+++ website/src/pages/arcade.tsx
@@ -6,8 +6,8 @@ export default function ArcadePage() {
   return (
     <BrowserOnly
       fallback={
-        <div className="fixed inset-0 flex items-center justify-center">
-          <HiOutlineCog className="text-3xl animate-spin" />
+        <div className="fixed inset-0 flex items-center justify-center bg-white dark:bg-zinc-900">
+          <HiOutlineCog className="text-3xl animate-spin text-gray-900 dark:text-gray-100" />
         </div>
       }
     >
