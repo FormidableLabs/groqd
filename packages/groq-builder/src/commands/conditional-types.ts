d035d5a0b7618a7a601e3210f8d3b5f743080bca
scottrippey <scott.william.rippey@gmail.com>
2023-12-23 21:21:34 -0600

    feature(conditionals): added conditional type helpers

---
+++ packages/groq-builder/src/commands/conditional-types.ts
@@ -0,0 +1,66 @@
+import {
+  ExtractProjectionResult,
+  ProjectionMap,
+  ProjectionMapOrCallback,
+} from "./projection-types";
+import { Empty, Simplify, Tagged, ValueOf } from "../types/utils";
+import { ExtractTypeNames, RootConfig } from "../types/schema-types";
+
+export type ConditionalProjections<TResultItem> = {
+  [Condition in ConditionalExpression<TResultItem>]: ProjectionMap<TResultItem>;
+};
+
+export type ConditionalExpression<TResultItem> = Tagged<string, TResultItem>;
+
+export type WrapConditionalProjectionResults<
+  TConditionalProjections extends ConditionalProjections<any>
+> = TConditionalProjections extends ConditionalProjections<infer TResultItem>
+  ? ConditionalProjectionResultWrapper<
+      ValueOf<{
+        [Condition in keyof TConditionalProjections]: Simplify<
+          ExtractProjectionResult<
+            TResultItem,
+            TConditionalProjections[Condition]
+          >
+        >;
+      }>
+    >
+  : never;
+
+declare const ConditionalProjectionResultTypes: unique symbol;
+export type ConditionalProjectionResultWrapper<TResultTypes> = {
+  [ConditionalProjectionResultTypes]: TResultTypes;
+};
+
+export type ExtractConditionalProjectionTypes<TResultItem> =
+  TResultItem extends ConditionalProjectionResultWrapper<infer TResultTypes>
+    ? TResultTypes
+    : Empty;
+
+export type ConditionalByTypeProjections<
+  TResultItem,
+  TRootConfig extends RootConfig
+> = {
+  [_type in ExtractTypeNames<TResultItem>]: ProjectionMapOrCallback<
+    TResultItem,
+    TRootConfig
+  >;
+};
+
+export type ExtractConditionalByTypeProjectionResults<
+  TConditionalProjections extends ConditionalByTypeProjections<any, any>
+> = TConditionalProjections extends ConditionalByTypeProjections<
+  infer TResultItem,
+  any
+>
+  ? ConditionalProjectionResultWrapper<
+      ValueOf<{
+        [_type in keyof TConditionalProjections]: TConditionalProjections[_type] extends ProjectionMapOrCallback<
+          infer TProjectionMap,
+          any
+        >
+          ? ExtractProjectionResult<TResultItem, TProjectionMap>
+          : never;
+      }>
+    >
+  : never;
