c936ab4efeeb8eb1ce0df812ea51097a59aeba93
scottrippey <scott.william.rippey@gmail.com>
2023-12-28 14:33:56 -0600

    feature(conditionals): added jsdocs

--- packages/groq-builder/src/commands/functions/value.ts
+++ packages/groq-builder/src/commands/functions/value.ts
@@ -4,16 +4,21 @@ import { Parser } from "../../types/public-types";
 declare module "../../groq-builder" {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   export interface GroqBuilder<TResult, TRootConfig> {
+    /**
+     * Returns a literal Groq value, properly escaped.
+     * @param value
+     * @param validation
+     */
     value<T extends LiteralValueTypes>(
       value: T,
-      validation?: Parser<T, T>
+      validation?: Parser<T, T> | null
     ): GroqBuilder<T, TRootConfig>;
   }
 }
 
 GroqBuilder.implement({
-  value(this: GroqBuilder, value) {
-    return this.chain(escapeValue(value));
+  value(this: GroqBuilder, value, validation = null) {
+    return this.chain(escapeValue(value), validation);
   },
 });
 
