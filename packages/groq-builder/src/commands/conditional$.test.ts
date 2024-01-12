c1d70c8705fc1b6623da23a4a083c485b27c4b39
scottrippey <scott.william.rippey@gmail.com>
2024-01-12 10:34:00 -0600

    feature(conditionals): added tests for multiple conditionals

--- packages/groq-builder/src/commands/conditional$.test.ts
+++ packages/groq-builder/src/commands/conditional$.test.ts
@@ -75,4 +75,106 @@ describe("conditional$", () => {
     `
     );
   });
+
+  describe("multiple conditionals", () => {
+    describe("without using unique keys", () => {
+      const qIncorrect = q.star.filterByType("variant").project((qV) => ({
+        name: true,
+        ...qV.conditional$({
+          "price == msrp": {
+            onSale: q.value(false),
+          },
+          "price < msrp": {
+            onSale: q.value(true),
+            price: true,
+            msrp: true,
+          },
+        }),
+        // Here we're trying to spread another conditional,
+        // however, it will override the first one
+        // since we didn't specify a unique key:
+        ...qV.conditional$({
+          "second == condition": { price: true },
+        }),
+      }));
+
+      it("the type will be missing the first conditionals", () => {
+        expectType<InferResultType<typeof qIncorrect>>().toStrictEqual<
+          Array<{ name: string } | { name: string; price: number }>
+        >();
+      });
+      it("the query will also be missing the first conditionals", () => {
+        expect(qIncorrect.query).toMatchInlineSnapshot(`
+          "*[_type == \\"variant\\"] {
+              name,
+              second == condition => {
+                  price
+                }
+            }"
+        `);
+      });
+    });
+
+    describe("with different keys", () => {
+      const qMultipleConditions = q.star
+        .filterByType("variant")
+        .project((qV) => ({
+          name: true,
+          ...qV.conditional$({
+            "price == msrp": {
+              onSale: q.value(false),
+            },
+            "price < msrp": {
+              onSale: q.value(true),
+              price: true,
+              msrp: true,
+            },
+          }),
+          ...qV.conditional$(
+            {
+              "another == condition": { foo: q.value("FOO") },
+            },
+            "unique-key"
+          ),
+        }));
+
+      it("the types should be inferred correctly", () => {
+        expectType<InferResultType<typeof qMultipleConditions>>().toStrictEqual<
+          Array<
+            | { name: string }
+            | { name: string; onSale: false }
+            | { name: string; onSale: true; price: number; msrp: number }
+            | { name: string; foo: "FOO" }
+            | { name: string; onSale: false; foo: "FOO" }
+            | {
+                name: string;
+                onSale: true;
+                price: number;
+                msrp: number;
+                foo: "FOO";
+              }
+          >
+        >();
+      });
+
+      it("the query should be compiled correctly", () => {
+        expect(qMultipleConditions.query).toMatchInlineSnapshot(`
+          "*[_type == \\"variant\\"] {
+              name,
+              price == msrp => {
+                  \\"onSale\\": false
+                },
+              price < msrp => {
+                  \\"onSale\\": true,
+                  price,
+                  msrp
+                },
+              another == condition => {
+                  \\"foo\\": \\"FOO\\"
+                }
+            }"
+        `);
+      });
+    });
+  });
 });
