b0f89482d601739d170e8342a31073fdab5ea759
scottrippey <scott.william.rippey@gmail.com>
2024-12-16 11:45:10 -0700

    docs: lint fix

--- packages/groqd/src/makeSafeQueryRunner.test.ts
+++ packages/groqd/src/makeSafeQueryRunner.test.ts
@@ -5,9 +5,13 @@ import { runPokemonQuery } from "../test-utils/runQuery";
 import invariant from "tiny-invariant";
 
 describe("makeSafeQueryRunner", () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const fn = vi.fn((query: string, params?: Record<string, unknown>) =>
+    Promise.resolve([])
+  );
+
   it("should create a query runner with single argument", async () => {
-    const fn = vi.fn(() => Promise.resolve<any[]>([]));
-    const runQuery = makeSafeQueryRunner(fn);
+    const runQuery = makeSafeQueryRunner((q) => fn(q));
 
     const res = await runQuery(q("*").filter().grab({ name: q.string() }));
     expect(fn).toHaveBeenCalledWith(`*[]{name}`);
@@ -15,10 +19,6 @@ describe("makeSafeQueryRunner", () => {
   });
 
   it("should create a query runner with additional args defined by user", async () => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const fn = vi.fn((query: string, params: Record<string, unknown>) =>
-      Promise.resolve<any[]>([])
-    );
     const runQuery = makeSafeQueryRunner(
       (query, params: Record<string, unknown>) => fn(query, params)
     );
