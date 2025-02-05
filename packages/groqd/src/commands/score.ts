0acbf19469d7b6c1ce270bf0392228fba5221bbd
scottrippey <scott.william.rippey@gmail.com>
2025-02-05 15:46:59 -0700

    feat(score): improved type def for `score`

--- packages/groqd/src/commands/score.ts
+++ packages/groqd/src/commands/score.ts
@@ -1,26 +1,27 @@
 import { GroqBuilder } from "../groq-builder";
 import { ResultItem } from "../types/result-types";
 import { Expressions } from "../types/groq-expressions";
-import { StringKeys } from "../types/utils";
 declare module "../groq-builder" {
   export interface GroqBuilder<TResult, TQueryConfig> {
     /**
      * Used to pipe a list of results through the score GROQ function.
      */
-    score<TKeys extends StringKeys<keyof ResultItem.Infer<TResult>>>(
-      ...scoringString: Array<
-        Expressions.Matching<ResultItem.Infer<TResult>, TQueryConfig> | TKeys
+    score(
+      ...scoringExpressions: Array<
+        Expressions.Score<ResultItem.Infer<TResult>, TQueryConfig>
       >
     ): GroqBuilder<
-      Array<ResultItem.Infer<TResult> & { _score: string }>,
+      ResultItem.Override<
+        TResult,
+        ResultItem.Infer<TResult> & { _score: number }
+      >,
       TQueryConfig
     >;
   }
 }
 
 GroqBuilder.implement({
-  score(this: GroqBuilder, ...scoringString) {
-    const query = ` | score(${scoringString.join(", ")})`;
-    return this.chain(query);
+  score(this: GroqBuilder, ...scoringExpressions) {
+    return this.chain(` | score(${scoringExpressions.join(", ")})`);
   },
 });
