f2a06b8892aa1eefdba03021c94dd39982d10b0b
scottrippey <scott.william.rippey@gmail.com>
2025-02-05 16:11:54 -0700

    feat(score): added more tests

--- packages/groqd/src/commands/score.test.ts
+++ packages/groqd/src/commands/score.test.ts
@@ -1,59 +1,118 @@
-import { createGroqBuilderWithZod } from "../index";
-import { SchemaConfig } from "../tests/schemas/nextjs-sanity-fe";
-import { describe, expect, it } from "vitest";
+import { InferResultType } from "../index";
+import { q } from "../tests/schemas/nextjs-sanity-fe";
+import { describe, expect, expectTypeOf, it } from "vitest";
 import { mock } from "../tests/mocks/nextjs-sanity-fe-mocks";
 import { executeBuilder } from "../tests/mocks/executeQuery";
-
-const q = createGroqBuilderWithZod<SchemaConfig>();
 
 const qVariants = q.star.filterByType("variant");
 
 describe("score", () => {
   const data = mock.generateSeedData({
-    variants: mock.array(10, (i) =>
-      mock.variant({
-        name: i === 5 ? `searched` : `Variant ${i}`,
-      })
-    ),
+    variants: [
+      mock.variant({ name: "one fish, two fish, red fish, blue fish" }),
+      mock.variant({ name: "the rainbow fish" }),
+      mock.variant({ name: "other" }),
+    ],
   });
 
   it("score is compiled correctly", () => {
-    const qScore = qVariants.score('name match "some name"');
-    expect(qScore).toMatchObject({
-      query: `*[_type == "variant"] | score(name match "some name")`,
-    });
+    const qScore = qVariants.score('name match "fish"');
+    expect(qScore.query).toMatchInlineSnapshot(
+      `"*[_type == "variant"] | score(name match "fish")"`
+    );
   });
 
   it("score is compiled correctly with multiple inputs", () => {
-    const qScore = qVariants.score('name match "test"', 'name match "hi"');
-    expect(qScore).toMatchObject({
-      query: `*[_type == "variant"] | score(name match "test", name match "hi")`,
-    });
+    const qScore = qVariants.score('name match "one"', 'name match "fish"');
+    expect(qScore.query).toMatchInlineSnapshot(
+      `"*[_type == "variant"] | score(name match "one", name match "fish")"`
+    );
   });
 
-  const scoredDataDesc = [
-    data.variants[5],
-    ...data.variants.slice(0, 5),
-    ...data.variants.slice(6),
-  ];
+  const qScore = qVariants
+    .score('name match "fish"')
+    .order("_score desc")
+    .project({
+      name: true,
+      _score: true,
+    });
 
-  const scoredDataAsc = [
-    ...data.variants.slice(0, 5),
-    ...data.variants.slice(6),
-    data.variants[5],
-  ];
+  it("should have the correct type", () => {
+    expectTypeOf<InferResultType<typeof qScore>>().toEqualTypeOf<
+      Array<{
+        name: string;
+        _score: number;
+      }>
+    >();
+  });
 
   it("should execute correctly with ordering desc", async () => {
-    const qScore = qVariants
-      .score('name match "searched"')
-      .order("_score desc");
     const results = await executeBuilder(qScore, data);
-    expect(results).toMatchObject(scoredDataDesc);
+    expect(results).toMatchInlineSnapshot(`
+      [
+        {
+          "_score": 1.6923076923076923,
+          "name": "one fish, two fish, red fish, blue fish",
+        },
+        {
+          "_score": 1,
+          "name": "the rainbow fish",
+        },
+        {
+          "_score": 0,
+          "name": "other",
+        },
+      ]
+    `);
   });
 
   it("should execute correctly with ordering asc", async () => {
-    const qScore = qVariants.score('name match "searched"').order("_score asc");
-    const results = await executeBuilder(qScore, data);
-    expect(results).toMatchObject(scoredDataAsc);
+    const qScoreDesc = qVariants
+      .score('name match "fish"')
+      .order("_score asc")
+      .project({
+        name: true,
+        _score: true,
+      });
+
+    const results = await executeBuilder(qScoreDesc, data);
+    expect(results).toMatchInlineSnapshot(`
+      [
+        {
+          "_score": 0,
+          "name": "other",
+        },
+        {
+          "_score": 1,
+          "name": "the rainbow fish",
+        },
+        {
+          "_score": 1.6923076923076923,
+          "name": "one fish, two fish, red fish, blue fish",
+        },
+      ]
+    `);
+  });
+
+  describe("when projecting", () => {
+    it("should execute correctly", async () => {
+      const results = await executeBuilder(qScore, data);
+      expect(results).toMatchInlineSnapshot(`
+        [
+          {
+            "_score": 1.6923076923076923,
+            "name": "one fish, two fish, red fish, blue fish",
+          },
+          {
+            "_score": 1,
+            "name": "the rainbow fish",
+          },
+          {
+            "_score": 0,
+            "name": "other",
+          },
+        ]
+      `);
+    });
   });
 });
