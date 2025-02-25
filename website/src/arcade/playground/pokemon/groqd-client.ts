d96a6ca4a08e590c8038e1642dbc2632d0f37dd5
scottrippey <scott.william.rippey@gmail.com>
2025-02-25 11:08:54 -0700

    feature(zod): change imports to `z`

--- website/src/arcade/playground/pokemon/groqd-client.ts
+++ website/src/arcade/playground/pokemon/groqd-client.ts
@@ -1,7 +1,7 @@
 import { createGroqBuilderWithZod, makeSafeQueryRunner } from "groqd";
 import type * as SanityTypes from "./pokemon.sanity.types";
 
-export { zod } from "groqd";
+export { z } from "groqd";
 
 type SchemaConfig = {
   schemaTypes: SanityTypes.AllSanitySchemaTypes;
